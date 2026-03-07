fcee8d81540fa3cd4980def0091c3de42f011743
madelynmleahu <50344146+madelynmleahu@users.noreply.github.com>
2026-03-06 21:56:41 -0800

    css changes and content additions
    added more sections, CSS updates,  more responsive web design

--- JS/pageload.js
+++ JS/pageload.js
@@ -1,4 +1,16 @@
-$(window).load(function() {
-		// Animate loader off screen
-		$(".pre-load").delay(5000).fadeOut("slow");
-	});
+// $(window).load(function() {
+// 		// Animate loader off screen
+// 		$(".pre-load").delay(3000).fadeOut("slow");
+// 	});
+
+window.addEventListener("load", () => {
+	setTimeout(() => {
+    const loader = document.querySelector(".pre-load");
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.6s ease";
+
+    setTimeout(() => {
+    loader.style.display = "none";
+    }, 600);
+	}, 3000);
+});
