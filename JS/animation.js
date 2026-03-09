e4f4c4c3874ee0ab20bab13c36ee80ca5eae1b6a
madelynmleahu <50344146+madelynmleahu@users.noreply.github.com>
2026-03-09 00:04:13 -0700

    beauty-fying
    polishing css

--- JS/animation.js
+++ JS/animation.js
@@ -1,33 +1,21 @@
-// drawLines();
+/* CURSOR GLOW EFFECT */
 
-// function getHeight(){
-//   return window.innerHeight
-// || document.documentElement.clientHeight
-// || document.body.clientHeight;
-// }
+const glow = document.querySelector('.cursor-glow');
 
-// function drawLines(){
-//   var lines = document.getElementsByClassName('line');
-//   if(lines.length) {
-//     for (var i = 0; i < lines.length; i++) {
-//         document.body.removeChild(lines[i]);
-//     }
-//   }
+let mouseX = 0;
+let mouseY = 0;
 
-//   var height = getHeight();
-//   for(i=0; i< height/10; i++){
-//     var line = document.createElement("div");
-//     line.className = "line line-" + i;
-//     line.style.top = i * 10 + "px";
-//     var time = Math.random() * 5;
-//     line.style.animation = "lines " + time + "s infinite";
-//     document.body.appendChild(line) ;
-//   }
-// }
+window.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX - 110;
+  mouseY = e.clientY - 110;
+});
 
-// window.onresize = function(event) {
-//   drawLines();
-// };
+function animate() {
+  glow.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  requestAnimationFrame(animate);
+}
+
+animate();
 
 /* SCOLL BOUNCE EFFECT */
 
